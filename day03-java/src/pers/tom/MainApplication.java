8cd38b62f06544c7c7d12373282dea29b206067d
liangkuaiqianderoubaozi <360788929@qq.com>
2021-06-10 09:17:27 +0800

    第二课

---
+++ day03-java/src/pers/tom/MainApplication.java
@@ -0,0 +1,20 @@
+package pers.tom;
+
+import java.util.ArrayList;
+
+public class MainApplication {
+
+
+    final int a = 1;
+
+    final static int b = 2;
+
+    final static ArrayList c = new ArrayList();
+
+    public static void main(String[] args) {
+        int b = 8>>2;
+        int c = 16>>3;
+
+        System.out.println(c);
+    }
+}
